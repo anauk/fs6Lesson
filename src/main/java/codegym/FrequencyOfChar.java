8c84c44a3cae4e3c342ba7716cf38528096dc780
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-05-13 16:00:30 +0300

    FrequencyOfChar

--- src/main/java/codegym/FrequencyOfChar.java
+++ src/main/java/codegym/FrequencyOfChar.java
@@ -7,6 +7,7 @@ public class FrequencyOfChar {
         Scanner in = new Scanner(System.in);
         char c = in.next().charAt(0);
         String str = in.next();
+
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if(str.charAt(i)==c){
