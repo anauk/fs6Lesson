18db02283c7d20af1c8015ae1e7deef744a58080
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-05-18 20:29:38 +0300

    Fibonacci v1

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -5,7 +5,21 @@ public class Fibonacci {
         System.out.print("Enter the number:");
         Scanner in = new Scanner(System.in);
         final int N = in.nextInt();
+
         int fibo = fibo(N);
-        System.out.printf("The fibonacci %d-th number is%d\n",N,fibo);
+
+        System.out.printf("The fibonacci %d-th number is: %d\n",N,fibo);
+    }
+
+    private static int fibo(int n) {
+        if (n==0) {
+            return 0;
+        } else if (n==1) {
+            return 1;
+        } else {
+            return fibo(n-1)+fibo(n-2);
+        }
     }
 }
+
+
