ed900bc455d50db821deb5193341879b49d0e80f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-06-02 11:37:21 +0300

    simples added and project structure

--- src/main/java/codegym/CharactersCoordinates.java
+++ src/main/java/codegym/CharactersCoordinates.java
@@ -3,7 +3,7 @@ package codegym;
 import java.util.*;
 
 public class CharactersCoordinates {
-    public static void main(String[] args) {
+    public static void main_class(String[] args) {
         String input="This is test string";
         Scanner in = new Scanner(
                 input
@@ -12,7 +12,6 @@ public class CharactersCoordinates {
         HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
         String line = in.nextLine();
         PriorityQueue<Character> pqKeys = new PriorityQueue<>();
-        //PriorityQueue<Map.Entry<Character, List<Integer>>> entries = new PriorityQueue<>((o1, o2) -> o1.getKey()-o2.getKey());
         for (int i = 0; i < line.length(); i++){
             char symbol = line.charAt(i);
             if (Character.isLetter(symbol)) {
@@ -34,6 +33,30 @@ public class CharactersCoordinates {
         while (!pqKeys.isEmpty()) {
             Character currentKey = pqKeys.poll();
             System.out.printf("%s=%s%s",currentKey, map.get(currentKey), pqKeys.isEmpty()?"":", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        String input="This is test string";
+        Scanner in = new Scanner(
+                input
+                //System.in
+        );
+        String line = in.nextLine();
+        HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < line.length(); i++){
+            char symbol = line.charAt(i);
+            if (Character.isLetter(symbol)) {
+                ArrayList<Integer> positions = map.getOrDefault(symbol, new ArrayList<>());
+                positions.add(i);
+                map.put(symbol, positions);
+            }
+        }
+        PriorityQueue<Map.Entry<Character, List<Integer>>> outcome = new PriorityQueue<>((o1, o2) -> o1.getKey()-o2.getKey());
+        map.forEach((c, l) -> outcome.add(new AbstractMap.SimpleEntry<>(c, l)));
+        while (!outcome.isEmpty()) {
+            Map.Entry e = outcome.poll();
+            System.out.printf("%s=%s%s",e.getKey(), e.getValue(), outcome.isEmpty()?"":", ");
         }
     }
 }
