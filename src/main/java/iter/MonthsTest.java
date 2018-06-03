e510587ba9f5bdea361376801988e17b6d43e3e3
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-06-03 11:12:52 +0300

    collections

--- src/main/java/iter/MonthsTest.java
+++ src/main/java/iter/MonthsTest.java
@@ -9,25 +9,23 @@ public class MonthsTest {
 
     public static void main1(String[] args) {
         Months months = new Months();
-
-        //----------------------------
         Iterator<String> iter = months.iterator();
         while (iter.hasNext()) {
             String m = iter.next();
             System.out.println(m);
         }
-        //----------------------------
-/*
+    }
+
+    public static void main2(String[] args) {
+        Months months = new Months();
         for (String m : months) {
             System.out.println(m);
         }
-*/
-
     }
 
     public static void main(String[] args) {
         Months m = new Months();
-        //m.forEach(s -> System.out.println(s));
+        m.forEach(s -> System.out.println(s));
         m.forEach(System.out::println);
         m.forEach(MonthsTest::m1);
     }
