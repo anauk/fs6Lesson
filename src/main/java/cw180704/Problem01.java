04059bbd86c8c3424f765774da16b665c0e66c8b
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-07-04 20:51:55 +0300

    io

--- src/main/java/cw180704/Problem01.java
+++ src/main/java/cw180704/Problem01.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 public class Problem01 {
 
-    private static int convert1(String s) {
-        return 0;
+    private static int convert(String s) {
+        return convert(s,-1);
     }
 
-    private static int convert2(String s, int defValue) {
-        return 0;
+    private static int convert(String s, int defValue) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return defValue;
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
 
-        int converted1 = convert1(s);
-        int converted2 = convert2(s, -33);
+        int converted1 = convert(s);
+        int converted2 = convert(s, -33);
 
+        System.out.println(converted1);
+        System.out.println(converted2);
 
-        int i = Integer.parseInt(s);
-        System.out.println(i);
+        //System.err.println(1);
     }
 
 }
