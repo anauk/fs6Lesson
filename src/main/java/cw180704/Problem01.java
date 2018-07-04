b24179174fa19aeb7fc9f2622c16885fbab64589
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-07-04 19:38:51 +0300

    io

---
+++ src/main/java/cw180704/Problem01.java
@@ -0,0 +1,27 @@
+package cw180704;
+
+import java.util.Scanner;
+
+public class Problem01 {
+
+    private static int convert1(String s) {
+        ;
+    }
+
+    private static int convert2(String s, int defValue) {
+        ;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        int converted1 = convert1(s);
+        int converted2 = convert2(s, -33);
+
+
+        int i = Integer.parseInt(s);
+        System.out.println(i);
+    }
+
+}
