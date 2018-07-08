73f7a1b677e8e821bbfa92f393a61c9f90876d74
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-07-08 15:57:00 +0300

    timetable

--- src/main/java/cw180709/TimetablePrintable.java
+++ src/main/java/cw180709/TimetablePrintable.java
@@ -1,14 +1,12 @@
 package cw180709;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class TimetablePrintable {
     private final Map<Integer, City> cities;
     private final Map<String, TimetableEntry> timetable;
-    private final int[] widths = {8,50,50,10};
-    private final int totalWidth;
+    private final int[] widths = {8, 0, 0};
     private final String header;
     private final String delimiter;
 
@@ -18,32 +16,33 @@ public class TimetablePrintable {
         this.widths[1] = cities.maxLength();
         this.widths[2] = cities.maxLength();
         this.header = generateHeader();
-        this.totalWidth = this.header.length();
-        this.delimiter = Stream.generate(()->"-").limit(totalWidth).reduce("",(a,b)->(a+b));
+        final int totalWidth = this.header.length();
+        System.out.println(totalWidth);
+        this.delimiter = Stream.generate(()->"-").limit(totalWidth).reduce("",(a, b)->(a+b));
     }
 
-    private String format0(int w1, int w2, int w3, int w4) {
-        return "%-" + w1 + "s %-" + w2 + "s-> %-" + w3 + "s %" + w4 + "s\n";
+    private String format0(int w1, int w2, int w3) {
+        return "%-" + w1 + "s %-" + w2 + "s-> %-" + w3 + "s %s";
     }
 
     private String format() {
-        return format0(widths[0],widths[1],widths[2],widths[3]);
-    }
-
-    private void printLine() {
-        System.out.println(delimiter);
+        return format0(widths[0],widths[1],widths[2]);
     }
 
     private String generateHeader() {
         return String.format(format(), "FlightNo", "From", "To", "Duration");
     }
 
+    private void printLine() {
+        System.out.println(delimiter);
+    }
+
     private void printHeader() {
-        System.out.print(this.header);
+        System.out.println(this.header);
     }
 
     private void printBody() {
-        timetable.forEach((s, tte) -> System.out.printf(format(),
+        timetable.forEach((s, tte) -> System.out.printf(format()+"\n",
                 tte.flightNo(),
                 cities.get(tte.src()).name(),
                 cities.get(tte.dst()).name(),
