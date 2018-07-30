21ea15299e42456ee54874de4b94a4ebabdae2e9
Bogdan Lyamzin <bogdan.lyamzin.d@gmail.com>
2018-07-30 09:01:28 +0000

    Upload New File

---
+++ src/main/frontend/html_css/lesson_29.07.18/BasicJS/basic.js
@@ -0,0 +1,21 @@
+
+// Одиночный комментарий
+/*
+
+Многострочный комментарий
+Еще продолжается
+*/
+
+let name = "Леся";
+
+
+for(let i = 0; i<2; i++) {
+    let name = "НеЛеся";
+}
+
+const _ = "Оля";
+
+let answer = prompt("Ваш возраст?","");
+answer = Number(answer) + "200";
+console.log(answer);
+
