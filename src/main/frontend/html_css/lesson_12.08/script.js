7f00e38c1150f170136e8b5939a6b3e7e161721a
Bogdan Lyamzin <bogdan.lyamzin.d@gmail.com>
2018-08-12 13:37:16 +0000

    Upload New File

---
+++ src/main/frontend/html_css/lesson_12.08/script.js
@@ -0,0 +1,22 @@
+//test();
+//
+//let test = function () {
+//    alert("22");
+//}
+//
+//function test() {
+//    alert("22");
+//}
+
+let user = {
+"first name": "John",  
+    age: 30
+};
+
+let name = "first name";
+
+console.log(user[name]);
+
+if("last name" in user) {
+    console.log("Есть кабан!");
+}
