8f37b6041eaa76853fc50b79b05572bd87f113ac
Bogdan Lyamzin <bogdan.lyamzin.d@gmail.com>
2018-08-12 13:36:58 +0000

    Upload New File

---
+++ src/main/frontend/html_css/lesson_12.08/createObject.js
@@ -0,0 +1,18 @@
+//Write the following code, one line for each action:
+//Create an empty object product
+//Add the property name with the value ’Laptop’
+//Add the property price with the value 1200
+//Change the value of the price to 1000
+//Show the product’s name and price on the screen
+//Remove the property name from the object
+
+let product = {};
+product.name = 'laptop';
+product.price = 1200;
+
+console.log(product);
+
+product.price = 1000;
+
+console.log(product);
+
