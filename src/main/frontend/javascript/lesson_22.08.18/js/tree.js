908a59e06f7212f47d3df3111eedf4973c54a1a8
Bogdan Lyamzin <bogdan.lyamzin.d@gmail.com>
2018-08-29 13:03:10 +0000

    Upload New File

---
+++ src/main/frontend/javascript/lesson_22.08.18/js/tree.js
@@ -0,0 +1,6 @@
+//Create a tree that shows/hides node children on click:
+//Requirements:
+//Only one event handler (use delegation)
+//A click outside a node title should not do anything
+
+let tree = document.querySelector("tree");
