a8a7fbe9c44648e80b9fde3757ce3452a84afcfb
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-09-14 20:14:24 +0300

    simples added and project structure

--- src/main/java/cw180912/Reader002.java
+++ src/main/java/cw180912/Reader002.java
@@ -3,46 +3,13 @@ package cw180912;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class Reader001 {
+public class Reader002 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File("src/main/java/cw180912/str.txt")));
         ArrayList<String> strings = new ArrayList<>();
-        Stream<String> lines = br.lines();
-
-        Map<Integer, List<String>> collect = lines.collect(Collectors.groupingBy(new Function<String, Integer>() {
-            @Override
-            public Integer apply(String s) {
-                return s.length();
-            }
-        }, Collectors.toList()));
-
-/*
-        collect.forEach(new BiConsumer<Integer, List<String>>() {
-            @Override
-            public void accept(Integer key, List<String> strings) {
-                try {
-                    BufferedWriter bw = new BufferedWriter(new FileWriter(new File(String.format("src/main/java/cw180912/%d.txt", key))));
-                    strings.forEach(s -> {
-                        try {
-                            bw.write(s);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-*/
 
         while (br.ready()) {
             strings.add(br.readLine());
@@ -63,7 +30,36 @@ public class Reader001 {
             }
         }
 
-       items.entrySet().forEach(new Consumer<Map.Entry<Integer, ArrayList<String>>>() {
+        // v1
+/*
+        for (String s : strings) {
+            int len = s.length();
+            ArrayList<String> al = items.get(len);
+            if (al == null) {
+                al = new ArrayList<>();
+                items.put(len, al);
+            }
+            al.add(s);
+        }
+*/
+        // v2
+/*
+        for (String s : strings) {
+            items.getOrDefault(s.length(), new ArrayList<>()).add(s);
+        }
+*/
+
+        // v3
+/*
+        strings.forEach(s -> items.computeIfAbsent(s.length(), k -> new ArrayList<>()).add(s));
+*/
+        // v4
+/*
+        strings
+                .forEach(s -> items.getOrDefault(s.length(), new ArrayList<>()).add(s));
+*/
+
+        items.entrySet().forEach(new Consumer<Map.Entry<Integer, ArrayList<String>>>() {
             @Override
             public void accept(Map.Entry<Integer, ArrayList<String>> el) {
                 try {
